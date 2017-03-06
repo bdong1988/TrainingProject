d1cac7b6f17cf63449e8f4eb0704a2307e2b5a21
Huanhuan Dong <donghuanhuan1988@hotmail.com>
2017-03-06 19:36:09 +0800

    homework 02

--- 02/timer/02/js/site.js
+++ 02/timer/02/js/site.js
@@ -1,3 +1,17 @@
 'use strict'
 
 var interval = null;
+var textInput = null;
+var textlength = 0;
+function onStartRolling(){
+    textInput = document.getElementById("rollingText");
+    interval = setInterval(function () {
+        rollingText()
+    }, 300);
+}
+
+function rollingText() {
+    let length = textInput.value.length;
+    let text = textInput.value.substr(1, length -1) + textInput.value.substr(0,1);
+    textInput.value = text;
+}
