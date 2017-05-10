9c190013b5c38b008a9ea85877d96d6f5c9d9f86
Huanhuan Dong <donghuanhuan1988@hotmail.com>
2017-05-10 20:55:46 +0800

    table

--- Classes/08/practise 03/js/site.js
+++ Classes/08/practise 03/js/site.js
@@ -4,16 +4,14 @@
   var app = angular.module('app', []);
 
   app.controller('ctrl', function ($scope) {
+    $scope.tableContent = [{name:'A', age:'1'},{name:'B', age:'2'},{name:'C', age:'3'},{name:'D', age:'4'},{name:'E', age:'5'}];
   })
 
-  angular.module(['app']).directive('myDirective', function (){
+  angular.module(['app']).directive('myTable', function (){
     return{
       restrict: 'E',
-      template: '<input ng-model="myText" type="text"></input>',
+      template:'<table><tr><th>Name</th><th>age</th></tr><tr ng-repeat="item in tableContent"><td>{{item.name}}</td><td>{{item.age}}</td></tr></table>',
       replace: 'true',
-      scope:{
-        myText:'='
-      }
     }
   })
 }();
